3c69ffbaafb22c264461595d86ee03c11beaf4a4
wendy <dbwforever@gmail.com>
2020-05-04 16:43:03 -0400

    sticky nav links hightlight

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -8,7 +8,6 @@ let stickyHeader = new StickyHeader();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
 
-
 let mobileMenu = new MobileMenu();
 
 if (module.hot) {
