960fe1ed936804875407c5eef7220ab89ed01a33
wendy <dbwforever@gmail.com>
2020-05-08 15:25:52 -0400

    completed our build process configuration

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,8 +3,7 @@ import 'lazysizes'
 import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
 import StickyHeader from './modules/StickyHeader'
-
-
+ 
 new StickyHeader(); 
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,10 @@
+const currentTask = process.env.npm_lifecycle_event
 const path = require('path')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const fse = require ('fs-extra')
+ 
 
 const postCSSPlugins = [
   require('postcss-import'),
@@ -9,7 +15,91 @@ const postCSSPlugins = [
   require('autoprefixer')
 ]
 
-module.exports = {
+
+class RunAfterCompile {
+  apply(compiler) {
+    compiler.hooks.done.tap('Copy imagaes', function(){
+      fse.copySync('./app/assets/images', './docs/assets/images')
+    })
+  }
+}
+
+let cssConfig = { 
+    test: /\.css$/i,
+    use: [ 'css-loader?url=false', {loader: 'postcss-loader', options: {plugins: postCSSPlugins}}] 
+}
+
+
+let pages = fse.readdirSync('./app').filter(function(file){
+  return file.endsWith('.html')
+}).map(function(page){
+  return new HtmlWebpackPlugin({
+    filename: page,
+    template: `./app/${page}`
+  })
+})
+
+let config = {
+  entry: './app/assets/scripts/App.js',
+  plugins: pages,
+  module: {
+    rules: [
+      cssConfig
+    ]
+  }
+}
+
+if (currentTask == 'dev') { 
+  cssConfig.use.unshift('style-loader')
+  config.output = {
+    filename: 'bundled.js',
+    path: path.resolve(__dirname, 'app')
+  }
+
+  config.devServer = {
+    before: function(app, server) {
+      server._watch('./app/**/*.html')
+    },
+    contentBase: path.join(__dirname, 'app'),
+    hot: true,
+    port: 3000,
+    host: '0.0.0.0'
+  }
+
+  config.mode = 'development'
+}
+
+if (currentTask == 'build') {
+  config.module.rules.push({
+    test: /\.js$/,
+    exclude: /(node_modules)/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env']
+      }
+    }
+  })
+  cssConfig.use.unshift(MiniCssExtractPlugin.loader)
+  postCSSPlugins.push(require('cssnano'))
+  config.output = {
+    filename: '[name].[chunkhash].js',
+    chunkFilename: '[name].[chunkhash].js',
+    path: path.resolve(__dirname, 'docs')
+  }
+  config.mode = 'production'
+  config.optimization = {
+    splitChunks: {chunks:'all'}
+  }
+  config.plugins.push(
+    new CleanWebpackPlugin(), 
+    new MiniCssExtractPlugin({filename: 'styles.[chunkhash].css'}),
+    new RunAfterCompile()
+    )
+}
+
+/* below is old code */
+let deletMeLater = {
   entry: './app/assets/scripts/App.js',
   output: {
     filename: 'bundled.js',
@@ -34,3 +124,5 @@ module.exports = {
     ]
   }
 }
+
+module.exports = config
